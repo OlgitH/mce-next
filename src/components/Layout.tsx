b9cfcbef762dc02b0d989162f711986be4786e13
OlgitH <berman.oj@gmail.com>
2024-07-15 17:05:26 +0100

    ~

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,6 +9,10 @@ type Props = {
   children: any;
 };
 export function Layout({ locales, navigation, settings, children }: Props) {
+  console.log("Header:", Header);
+  console.log("Footer:", Footer);
+  console.log("Socials:", Socials);
+
   return (
     <div className="text-slate-800">
       <Header locales={locales} navigation={navigation} settings={settings} />
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,15 +4,12 @@ function Footer() {
       <div className="container">
         <div className="py-28 flex flex-col items-center w-full">
           <h3 className="">
-            Website built by{" "}
+            Website built by
             <a href="https://www.aquicreative.com">AquiCreative</a>
           </h3>
-          <a
-            href="https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts"
-            className="mx-3 mb-6 lg:mb-0"
-          >
+          <p className="mx-3 mb-6 lg:mb-0">
             All rights reserved. Magic Coffee Expedition.
-          </a>
+          </p>
         </div>
       </div>
     </footer>
