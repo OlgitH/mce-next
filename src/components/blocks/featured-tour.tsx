7199d7f977c4d5ebacb2882309f39a0338b32142
OlgitH <berman.oj@gmail.com>
2024-11-07 17:54:49 +0000

    Update featured-tour.tsx

--- src/components/blocks/featured-tour.tsx
+++ src/components/blocks/featured-tour.tsx
@@ -22,7 +22,7 @@ const FeaturedTour = ({
   link,
 }: Props) => {
   return (
-    <div className="box-border grow overflow-hidden relative">
+    <div className="box-border grow overflow-hidden relative group">
       {link && (
         <Link href={link} className="p-10 left-0 w-full h-full">
           <div className="image relative h-[240px] w-full mb-4">
@@ -36,11 +36,12 @@ const FeaturedTour = ({
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               />
             )}
+            <div
+              style={{ backgroundColor: bgColour }}
+              className="transition-opacity duration-700 opacity-0 pointer-events-none group-hover:opacity-40 group-hover:pointer-events-auto w-full h-full z-10 absolute left-0 top-0"
+            ></div>
           </div>
-          {/* <div
-            style={{ backgroundColor: bgColour, opacity: opacity ?? 0.6 }}
-            className={`overlay w-full h-full z-10 absolute left-0 top-0`}
-          ></div> */}
+
           <div className="relative z-20">{children}</div>
         </Link>
       )}
