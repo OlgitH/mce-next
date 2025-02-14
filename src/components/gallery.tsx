579b7738335aca37e51b17e87759cec60c0544e7
OlgitH <berman.oj@gmail.com>
2025-02-14 14:54:40 +0000

    Update gallery.tsx

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -30,50 +30,58 @@ const Gallery = ({ images }: Props) => {
   };
 
   // Get the current image based on the currentSlide index
-  const currentImage = images[currentSlide].image;
+  const currentImage = images[currentSlide].image ?? null;
 
   return (
     <div className="container relative">
       {/* Full-width image with number text */}
-      <div className="mySlides relative">
-        <img src={currentImage.url} alt={currentImage.alt} className="w-full" />
-        {/* Next and previous buttons */}
-        <div className="z-40 flex absolute h-full right-0 top-0 items-center bg-[coral] sm:bg-transparent sm:hover:bg-[coral] transition duration-300 ease-in-out ">
-          <a
-            className="next px-2 text-white block h-full flex items-center  text-2xl z-10  cursor-pointer"
-            onClick={nextSlide}
-          >
-            &#10095;
-          </a>
-        </div>
-        <div className="z-40 flex absolute h-full left-0 top-0 items-center bg-[coral] sm:bg-transparent sm:hover:bg-[coral] transition duration-300 ease-in-out ">
-          <a
-            className="prev px-2 text-white block h-full flex items-center  text-2xl z-10 cursor-pointer"
-            onClick={prevSlide}
-          >
-            &#10094;
-          </a>
-        </div>
-      </div>
-
-      {/* Image caption */}
-      <div className="caption-container">
-        <p id="caption">{currentImage.alt}</p>
-      </div>
-
-      {/* Thumbnail images */}
-      <div className="thumbnail-container flex gap-2 overflow-x-auto mt-4">
-        {images.map((item, index) => (
-          <div key={index} className="thumbnail-column flex-shrink-0">
+      {currentImage && (
+        <>
+          <div className="mySlides relative">
             <img
-              className="demo cursor-pointer w-24"
-              src={item.image.url}
-              alt={item.image.alt}
-              onClick={() => setCurrentSlide(index)} // Clicking a thumbnail sets the current slide
+              src={currentImage.url}
+              alt={currentImage.alt}
+              className="w-full"
             />
+            {/* Next and previous buttons */}
+            <div className="z-40 flex absolute h-full right-0 top-0 items-center bg-[coral] sm:bg-transparent sm:hover:bg-[coral] transition duration-300 ease-in-out ">
+              <a
+                className="next px-2 text-white block h-full flex items-center  text-2xl z-10  cursor-pointer"
+                onClick={nextSlide}
+              >
+                &#10095;
+              </a>
+            </div>
+            <div className="z-40 flex absolute h-full left-0 top-0 items-center bg-[coral] sm:bg-transparent sm:hover:bg-[coral] transition duration-300 ease-in-out ">
+              <a
+                className="prev px-2 text-white block h-full flex items-center  text-2xl z-10 cursor-pointer"
+                onClick={prevSlide}
+              >
+                &#10094;
+              </a>
+            </div>
           </div>
-        ))}
-      </div>
+
+          {/* Image caption */}
+          <div className="caption-container">
+            <p id="caption">{currentImage.alt}</p>
+          </div>
+
+          {/* Thumbnail images */}
+          <div className="thumbnail-container flex gap-2 overflow-x-auto mt-4">
+            {images.map((item, index) => (
+              <div key={index} className="thumbnail-column flex-shrink-0">
+                <img
+                  className="demo cursor-pointer w-24"
+                  src={item.image.url}
+                  alt={item.image.alt}
+                  onClick={() => setCurrentSlide(index)} // Clicking a thumbnail sets the current slide
+                />
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
