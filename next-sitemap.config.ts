05aee12ececf893f7c165b7c6494612d565454a1
OlgitH <berman.oj@gmail.com>
2025-06-22 22:15:24 +0100

    add robots.txt and sitemap

---
+++ next-sitemap.config.ts
@@ -0,0 +1,17 @@
+module.exports = {
+  siteUrl: "https://www.magiccoffeeexpedition.co.uk",
+  generateRobotsTxt: true,
+  changefreq: "monthly",
+  priority: 0.7,
+  sitemapSize: 5000,
+  alternateRefs: [
+    {
+      href: "https://www.magiccoffeeexpedition.co.uk/en-gb",
+      hreflang: "en-gb",
+    },
+    {
+      href: "https://www.magiccoffeeexpedition.co.uk/es-co",
+      hreflang: "es-co",
+    },
+  ],
+};
